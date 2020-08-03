ab6bb4ea69bc306bb4962eea5924b2d2c41df21f
lollcat <44415540+lollcat@users.noreply.github.com>
2020-08-03 20:51:14 +0200

    minor updates

--- SAC/run_SAC.py
+++ SAC/run_SAC.py
@@ -2,7 +2,7 @@
 # tensorboard --logdir SAC/logs
 from Utils.BFD_maker import Visualiser
 from SAC.SAC_Agent.Agent import Agent
-SAC = Agent(total_eps=200, batch_size=64, max_mem_length=1000)
+SAC = Agent(total_eps=300, batch_size=132, max_mem_length=10000)
 #SAC = Agent(total_eps=200, batch_size=2)
 SAC.run()
 
