acb01e38b39a65332ee6e8038d23411c411ff6a2
lollcat <44415540+lollcat@users.noreply.github.com>
2020-08-03 20:53:31 +0200

    Delete playground.py

--- playground.py
+++
@@ -1,5 +0,0 @@
-# tensorboard --logdir logs
-from SAC.SAC_Agent.Agent import Agent
-#SAC = Agent(total_eps=300, batch_size=64, max_mem_length=1000)
-SAC = Agent(total_eps=200, batch_size=2)
-SAC.run()
